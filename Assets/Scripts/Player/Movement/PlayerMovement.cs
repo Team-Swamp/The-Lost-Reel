bc4e1e53ebdf4f7cca0d863d8d42963005b8b348
bas-boop <dereusbas461@gmail.com>
2023-08-07 16:56:24 +0200

    Changed moved to rigidbody velocity

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -3,25 +3,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
-
-    private CharacterController _characterController;
-
-
-    private void Start()
-    {
-        _characterController = gameObject.AddComponent<CharacterController>();
-    }
+    [SerializeField] private Rigidbody rb;
 
     private void Update()
     {
         MovementPlayer();
     }
 
-    public void MovementPlayer()
+    private void MovementPlayer()
     {
-        var horizontal = Input.GetAxis("Horizontal") * movementSpeed;
-        var vertical = Input.GetAxis("Vertical") * movementSpeed;
-        
-        _characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
+        var horizontal = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        var vertical = Input.GetAxisRaw("Vertical") * movementSpeed;
+
+        var move = new Vector3(horizontal, rb.velocity.y, vertical);
+        rb.velocity = move * movementSpeed * Time.deltaTime;
     }
 }
