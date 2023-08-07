3e58e71c636187cb379810c8fa8e13182b23eff5
Tatum13 <tatumdv2001@gmail.com>
2023-08-07 16:06:42 +0200

    added beta version playermovement
    Co-Authored-By: rickdohmen <70955143+rickdohmen@users.noreply.github.com>

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,8 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private float speed;
+
+    private CharacterController _characterController;
+
+
+    private void Start()
+    {
+        _characterController = gameObject.AddComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        MovementPlayer();
+    }
+
+    public void MovementPlayer()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _characterController.Move(move * Time.deltaTime * speed);
+
+        if (move != Vector3.zero) gameObject.transform.forward = move;
+    }
 }
