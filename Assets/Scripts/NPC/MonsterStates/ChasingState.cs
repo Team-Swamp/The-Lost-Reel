40625efc4eb73dabd1e7810ef132a4e212f16983
bas-boop <dereusbas461@gmail.com>
2023-08-09 15:05:30 +0200

    Trying to make a chase state

--- Assets/Scripts/NPC/MonsterStates/ChasingState.cs
+++ Assets/Scripts/NPC/MonsterStates/ChasingState.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class ChasingState : MonsterBaseState
 {
-    protected override void EnterState(MonsterStateMachine monster)
-    {
-        Debug.Log("Start chasing!");
-    }
+    protected override void EnterState(MonsterStateMachine monster) { }
 
-    protected override void UpdateState(MonsterStateMachine monster) { }
+    protected override void UpdateState(MonsterStateMachine monster)
+    {
+        // monster.Agent.SetDestination(monster.Player.transform.position);
+        // monster.Agent.Move(monster.Player.transform.position);
+        // monster.Agent.Warp(monster.Player.transform.position);
+        // monster.Agent.destination = monster.Player.transform.position;
+
+        // This al give a error ^^
+        // trying to update the monster its destination, because the player is running away. Maybe try something when playerPos != lastPlayerPos
+    }
     protected override void FixedUpdateState(MonsterStateMachine monster) { }
     protected override void ExitState(MonsterStateMachine monster) { }
 }
