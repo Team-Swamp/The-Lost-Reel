ca1122235bcf3d40b1c8e6e5b2ca3a10cdbd4944
rickdohmen <rickdohmen2003@gmail.com>
2023-08-09 15:33:52 +0200

    set some variables private and am adding a call function

--- Assets/Scripts/UI/WarningText.cs
+++ Assets/Scripts/UI/WarningText.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WarningText : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private enum alphaValue
+    {
+        SHRINKING,
+        GROWING,
+    }
+
+    private alphaValue currentAlphaValue;
+    [SerializeField] private float CommentminAlpha;
+    [SerializeField] private float CommentmaxAlpha;
+    [SerializeField] private float CommentCurrentAlpha;
+    [SerializeField] private bool flashText;
+
+    [SerializeField] private Text MyText;
+
     void Start()
     {
+        CommentminAlpha = 0f;
+        CommentmaxAlpha = 1.0f;
+        CommentCurrentAlpha = 1.0f;
+        currentAlphaValue = alphaValue.SHRINKING;
+    }
+
+    void Update()
+    {
+        if (flashText == true)
+        {
+            alphaComments();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetIsAllowedToFlash()
     {
-        
+
+    }
+
+    private void alphaComments()
+    {
+        if (currentAlphaValue == alphaValue.SHRINKING)
+        {
+            CommentCurrentAlpha = CommentCurrentAlpha - 0.005f;
+            MyText.color = new Color(Color.white.r, Color.white.g, Color.white.b, CommentCurrentAlpha);
+            if (CommentCurrentAlpha <= CommentminAlpha)
+            {
+                currentAlphaValue = alphaValue.GROWING;
+            }
+        }
+        else if (currentAlphaValue == alphaValue.GROWING)
+        {
+            CommentCurrentAlpha = CommentCurrentAlpha + 0.005f;
+            MyText.color = new Color(Color.white.r, Color.white.g, Color.white.b, CommentCurrentAlpha);
+            if (CommentCurrentAlpha >= CommentmaxAlpha)
+            {
+                currentAlphaValue = alphaValue.SHRINKING;
+            }
+        }
     }
 }
