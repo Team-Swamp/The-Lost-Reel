490238517329118f27408d24ff0dc2143b112395
Tatum13 <tatumdv2001@gmail.com>
2023-08-09 20:42:13 +0200

    added chased state to state list

--- Assets/Scripts/NPC/MonsterStates/ChasingState.cs
+++ Assets/Scripts/NPC/MonsterStates/ChasingState.cs
@@ -6,14 +6,9 @@ public class ChasingState : MonsterBaseState
 
     protected override void UpdateState(MonsterStateMachine monster)
     {
-        // monster.Agent.SetDestination(monster.Player.transform.position);
-        // monster.Agent.Move(monster.Player.transform.position);
-        // monster.Agent.Warp(monster.Player.transform.position);
-        // monster.Agent.destination = monster.Player.transform.position;
-
-        // This al give a error ^^
-        // trying to update the monster its destination, because the player is running away. Maybe try something when playerPos != lastPlayerPos
+        monster.Agent.SetDestination(monster.Player.transform.position);
     }
+    
     protected override void FixedUpdateState(MonsterStateMachine monster) { }
     protected override void ExitState(MonsterStateMachine monster) { }
 }
