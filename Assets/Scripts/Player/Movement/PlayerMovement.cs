9d923b4f77fe0a1d8bfc3f62cd43b60c16cebc9d
Tatum13 <tatumdv2001@gmail.com>
2023-08-11 01:38:51 +0200

    applied feedback

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public sealed class PlayerMovement : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float gravity;
-    [SerializeField] private float groundDistance;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundMask;
     
+    [Header("Player settings")]
+    [SerializeField] private float movementSpeed;
+    [SerializeField, Range(0, -10)] private float gravity;
+    [SerializeField, Range(0, 1)] private float groundDistance;
+
     private Vector3 _moveDirection;
     private Vector3 _velocity;
     private float _horizontal;
@@ -17,19 +20,29 @@ public sealed class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        SetInput();
+        ApplyGravity();
+        SetMoveDirection();
+    }
+
+    private void SetInput()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void ApplyGravity()
+    {
         _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
         if (_isGrounded && _velocity.y < 0) _velocity.y = 0f;
-            
-        _horizontal = Input.GetAxis("Horizontal");
-        _vertical = Input.GetAxis("Vertical");
-
-        _moveDirection = transform.right * _horizontal + transform.forward * _vertical;
-
-        characterController.Move(_moveDirection * movementSpeed * Time.deltaTime);
-
         _velocity.y += gravity * Time.deltaTime;
+    }
 
-        characterController.Move(_velocity * Time.deltaTime);
+    private void SetMoveDirection()
+    {
+        _moveDirection = (transform.right * _horizontal + transform.forward * _vertical) * movementSpeed;
+        _moveDirection.y = _velocity.y;
+        
+        characterController.Move(_moveDirection * Time.deltaTime);
     }
 }
