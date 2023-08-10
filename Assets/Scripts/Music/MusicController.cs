35784f98fe56cfc1a9c094f8a524aad4716cd5b9
Tatum13 <tatumdv2001@gmail.com>
2023-08-10 14:29:05 +0200

    added prefab and test clips

--- Assets/Scripts/Music/MusicController.cs
+++ Assets/Scripts/Music/MusicController.cs
@@ -1,39 +1,19 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
-using Random = UnityEngine.Random;
 
 public class MusicController : MonoBehaviour
 {
     [SerializeField] private AudioClip startMusic;
     [SerializeField] private AudioClip audio;
-    [SerializeField] private List<AudioClip> audioclips = new List<AudioClip>();
+    [SerializeField] private List<AudioClip> audioclips;
     [SerializeField] private AudioSource audioSource;
 
     private bool activeMusic;
 
     private void Start()
     {
-        var i = Random.Range(0, audioclips.Count);
-        audio = audioclips[i];
-        audioSource.clip = audio;
+        audioSource.clip = startMusic;
         audioSource.Play();
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            ChangeMusicToChase();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            ChangeMusicToIdle();
-        }
     }
     
     public void ChangeMusicToChase()
@@ -44,16 +24,11 @@ public class MusicController : MonoBehaviour
         audioSource.Play();
     }
 
-    public void ChangeMusicToIdle()
+    public void ChangeMusicToAmbience()
     {
         audioSource.Stop();
         audio = audioclips[0];
         audioSource.clip = audio;
         audioSource.Play();
-    }
-    
-    public void ChangeMusicToRandom()
-    {
-        
     }
 }
