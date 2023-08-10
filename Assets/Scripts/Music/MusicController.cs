a0264af972641dd0b6493b43eccbd360683ca5c2
Tatum13 <tatumdv2001@gmail.com>
2023-08-10 16:27:28 +0200

    added change music with one function

--- Assets/Scripts/Music/MusicController.cs
+++ Assets/Scripts/Music/MusicController.cs
@@ -1,34 +1,21 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicController : MonoBehaviour
+public sealed class MusicController : MonoBehaviour
 {
-    [SerializeField] private AudioClip startMusic;
-    [SerializeField] private AudioClip audio;
     [SerializeField] private List<AudioClip> audioclips;
     [SerializeField] private AudioSource audioSource;
-
-    private bool activeMusic;
-
-    private void Start()
-    {
-        audioSource.clip = startMusic;
-        audioSource.Play();
-    }
     
-    public void ChangeMusicToChase()
-    {
-        audioSource.Stop();
-        audio = audioclips[1];
-        audioSource.clip = audio;
-        audioSource.Play();
-    }
+    private AudioClip _audio;
 
-    public void ChangeMusicToAmbience()
+    private void Start() => ChangeMusic(0);
+
+    private void ChangeMusic(int clip)
     {
         audioSource.Stop();
-        audio = audioclips[0];
-        audioSource.clip = audio;
+        _audio = audioclips[clip];
+        audioSource.clip = _audio;
         audioSource.Play();
     }
 }
