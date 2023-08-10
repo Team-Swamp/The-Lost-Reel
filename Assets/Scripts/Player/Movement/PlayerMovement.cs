e4f71a4320ca4d14de148dfba3d7b0386f4baba7
Tatum13 <tatumdv2001@gmail.com>
2023-08-11 00:22:17 +0200

    added gravity to player with groundcheck

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -4,18 +4,32 @@ public sealed class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float gravity;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
     
     private Vector3 _moveDirection;
+    private Vector3 _velocity;
     private float _horizontal;
     private float _vertical;
+    private bool _isGrounded;
 
     private void Update()
     {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (_isGrounded && _velocity.y < 0) _velocity.y = 0f;
+            
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
 
         _moveDirection = transform.right * _horizontal + transform.forward * _vertical;
 
         characterController.Move(_moveDirection * movementSpeed * Time.deltaTime);
+
+        _velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(_velocity * Time.deltaTime);
     }
 }
