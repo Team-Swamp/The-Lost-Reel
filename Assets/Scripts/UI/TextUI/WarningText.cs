b9e4238542bc25064531dd31e16a4861bc73c070
bas-boop <dereusbas461@gmail.com>
2023-08-11 17:18:46 +0200

    Fixed fading when turned off

--- Assets/Scripts/UI/TextUI/WarningText.cs
+++ Assets/Scripts/UI/TextUI/WarningText.cs
@@ -21,21 +21,19 @@ public sealed class WarningText : MonoBehaviour
     {
         CommentminAlpha = 0f;
         CommentmaxAlpha = 1.0f;
-        CommentCurrentAlpha = 1.0f;
+        CommentCurrentAlpha = 0f;
         _currentAlphaValue = alphaValue.SHRINKING;
     }
 
     private void Update()
     {
-        if (isAllowedToflash == true)
+        if (isAllowedToflash)
         {
             alphaComments();
         }
         else
         {
-            _currentAlphaValue = alphaValue.SHRINKING;
-            CommentCurrentAlpha = CommentminAlpha;
-            text.color = new Color(Color.white.r, Color.white.g, Color.white.b, CommentminAlpha);
+            ShrinkAlpha();
         }
     }
     public void SetIsAllowedToFlash(bool targetValue)
@@ -62,6 +60,18 @@ public sealed class WarningText : MonoBehaviour
             {
                 _currentAlphaValue = alphaValue.SHRINKING;
             }
+        }
+    }
+    
+    private void ShrinkAlpha()
+    {
+        if (CommentCurrentAlpha <= 0) return;
+        
+        CommentCurrentAlpha -= flashingSpeed;
+        text.color = new Color(Color.white.r, Color.white.g, Color.white.b, CommentCurrentAlpha);
+        if (CommentCurrentAlpha >= CommentmaxAlpha)
+        {
+            _currentAlphaValue = alphaValue.SHRINKING;
         }
     }
 }
