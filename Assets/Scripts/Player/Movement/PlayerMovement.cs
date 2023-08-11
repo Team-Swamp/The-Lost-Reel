a149dec9ee485caa7ae472f37a8f44512233e56e
Tatum13 <tatumdv2001@gmail.com>
2023-08-11 14:36:51 +0200

    applied feedback

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -8,14 +8,14 @@ public sealed class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask groundMask;
     
     [Header("Player settings")]
-    [SerializeField, Range(0, 10)] private float defaultSpeed;
+    [SerializeField, Range(0, 10)] private float walkingSpeed;
     [SerializeField, Range(0, 10)] private float runningSpeed;
     [SerializeField, Range(0, -10)] private float gravity;
     [SerializeField, Range(0, 1)] private float groundDistance;
 
     private Vector3 _moveDirection;
     private Vector3 _velocity;
-    private float _movementSpeed;
+    private float _currentMovementSpeed;
     private float _horizontal;
     private float _vertical;
     private bool _isGrounded;
@@ -43,11 +43,11 @@ public sealed class PlayerMovement : MonoBehaviour
 
     private void SetMoveDirection()
     {
-        _moveDirection = (transform.right * _horizontal + transform.forward * _vertical) * _movementSpeed;
+        _moveDirection = (transform.right * _horizontal + transform.forward * _vertical) * _currentMovementSpeed;
         _moveDirection.y = _velocity.y;
         
         characterController.Move(_moveDirection * Time.deltaTime);
     }
 
-    private void ApplyRunning() => _movementSpeed = Input.GetKey(KeyCode.LeftShift) ? runningSpeed : defaultSpeed;
+    private void ApplyRunning() => _currentMovementSpeed = Input.GetKey(KeyCode.LeftShift) ? runningSpeed : walkingSpeed;
 }
