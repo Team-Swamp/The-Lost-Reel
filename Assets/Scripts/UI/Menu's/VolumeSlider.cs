ea2b025f5ade1a26aff4fbfb93a906666583bea1
rickdohmen <rickdohmen2003@gmail.com>
2023-08-13 14:57:15 +0200

    added a script to the volume slider and aaded it to the prefab

---
+++ Assets/Scripts/UI/Menu's/VolumeSlider.cs
@@ -0,0 +1,37 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
