d8f57af46704f7f6c60c7424ea6d35ba695eec80
rickdohmen <rickdohmen2003@gmail.com>
2023-08-13 17:38:59 +0200

    added an extra audiosource in the script

--- Assets/Scripts/UI/Menu's/VolumeSlider.cs
+++ Assets/Scripts/UI/Menu's/VolumeSlider.cs
@@ -5,6 +5,7 @@ public class VolumeSlider : MonoBehaviour
 {
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource audioSource2;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,7 @@ public class VolumeSlider : MonoBehaviour
     public void ChangeVolume()
     {
         audioSource.volume = volumeSlider.value;
+        audioSource2.volume = volumeSlider.value;
         Save();
     }
 
